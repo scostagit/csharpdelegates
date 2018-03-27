3a419088b9f16f2c8ee940dbec554a7b03d980ad
svcosta <sergio.@hotmail.com>
2018-03-27 23:58:13 +0100

    A assinatura de delegates: O operador +=: Para fazer a assinatura de um evento e adicionar um comportamento quando este evento acontecer, podemos utilizar o operador +=! Não podemos usar o operador = em eventos Delegate.Combine

---
+++ ByteBank.Agencias/EdicaoAgencia.xaml.cs
@@ -0,0 +1,88 @@
+﻿using ByteBank.Agencias.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ByteBank.Agencias
+{
+    /// <summary>
+    /// Interaction logic for EdicaoAgencia.xaml
+    /// </summary>
+    public partial class EdicaoAgencia : Window
+    {
+        private readonly Agencia _agencia;
+
+        public EdicaoAgencia(Agencia agencia)
+        {
+            InitializeComponent();
+            //se a agencia for nula lançaremos uma execção.
+            this._agencia = agencia ?? throw new ArgumentNullException(nameof(agencia));
+            this.AtualizarControles();
+            this.AtualizarCamposDeTexto();
+
+        }
+
+        private void AtualizarCamposDeTexto()
+        {
+            txtNumero.Text = this._agencia.Numero;
+            txtNome.Text = this._agencia.Nome;
+            txtTelefone.Text = this._agencia.Telefone;
+            txtEndereco.Text = this._agencia.Endereco;
+            txtDescricao.Text = this._agencia.Descricao;
+        }
+
+        //nesse metodo iremos assinar todos os eventos.
+        private void AtualizarControles()
+        {
+            //Duas formas de manipulas delegates. tudo funiona.
+
+            var okEventHandler = (RoutedEventHandler)btnOk_Click + Fechar;
+            //var cancelarEventHandler = (RoutedEventHandler)btnCancelar_Click + Fechar;
+
+            //Por baixo dos panos o codigo acima esta chando a classe Delegate e fazendo um combine
+            //delegate + delegate = delegate.Combine(a,b)
+            //public static Delegate Combine(Delegate a, Delegate b);
+
+
+            //var okEventHandler = (RoutedEventHandler)Delegate.Combine(
+            //                          (RoutedEventHandler)btnOk_Click, 
+            //                          (RoutedEventHandler)Fechar); 
+
+            var cancelarEventHandler = (RoutedEventHandler)Delegate.Combine(
+                                      (RoutedEventHandler)btnCancelar_Click,
+                                      (RoutedEventHandler)Fechar);
+
+
+            //mesma coisa que o codigo acima
+
+            //assinando os eventso do click
+            this.btnOk.Click += new RoutedEventHandler(btnOk_Click);
+            this.btnCancelar.Click += new RoutedEventHandler(btnCancelar_Click);
+
+            this.btnOk.Click += new RoutedEventHandler(Fechar);
+            this.btnCancelar.Click += new RoutedEventHandler(Fechar);
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)=>        
+            DialogResult = true;    
+        
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)=>        
+            DialogResult = false; 
+
+        private void Fechar(object sender, RoutedEventArgs e) =>
+            this.Close();
+        
+    }
+}
+
+
--- ByteBank.Agencias/MainWindow.xaml.cs
+++ ByteBank.Agencias/MainWindow.xaml.cs
@@ -16,21 +16,38 @@ using System.Windows.Shapes;
 
 namespace ByteBank.Agencias
 {
+    /*
+     * EVENTOS  E DELEGATES
+     
+        * O Delegate é uma estrutura de dados para a representação, fortemente tipada, da assinatura de um método: 
+        * seu tipo de retorno, a ordem e os tipos de seus parâmetros. 
+        * São componentes de um delegate o seu tipo de retorno, os parâmetros e também a ordem dos parâmetros!
+    
+        * O evento é uma mensagem de que algo aconteceu: seja um clique em um botão, uma mudança no item selecionado de uma lista ou uma 
+        pausa em um serviço do Windows. 
+        * Podemos assinar estes eventos por meio de delegates. 
+        * Para assinarmos um evento, é necessário usar um delegate com uma assinatura que combine com o método que o emissor do evento espera invocar. 
+        * Com isto, vemos a importância dos delegates para a representação da assinatura de métodos!    
+
+     */
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         private readonly ByteBankEntities _contextoBancoDeDados = new ByteBankEntities();
-        private readonly AgenciasListBox lstAgencias;
+        //private readonly AgenciasListBox lstAgencias;
+        private readonly ListBox lstAgencias;
 
         public MainWindow()
         {
             InitializeComponent();
 
             //this e dp tipo MainWindow.
-            lstAgencias = new AgenciasListBox(this);
+            // lstAgencias = new AgenciasListBox(this);
+            lstAgencias = new ListBox();
             AtualizarControles();
+            this.AtualizarListaDeAgengicas();
         }
 
         private void AtualizarControles()
@@ -41,13 +58,48 @@ namespace ByteBank.Agencias
             Canvas.SetTop(lstAgencias, 15);
             Canvas.SetLeft(lstAgencias, 15);
 
+            //atribuindo a lista de delegate.
+            lstAgencias.SelectionChanged += new SelectionChangedEventHandler(this.ListBox_SelectionChanged);
+
             //coisas do WPF vc tem o canvas e children: vc vai adicionar um novo filho. 
             container.Children.Add(lstAgencias);
 
+            this.btnEditar.Click += new RoutedEventHandler(this.btnEditar_Click);
+        }
+
+        private void btnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            var agenciaSelecionada = (Agencia)this.lstAgencias.SelectedItem;
+            var janelaEdicao = new EdicaoAgencia(agenciaSelecionada);
+            var resultado = janelaEdicao.ShowDialog().Value;
+
+            if (resultado)
+            {
+                //Usuario clicou em Ok
+            }
+            else
+            {
+                //Usuario clicou em cancelar
+            }
+        }
+
+        private void AtualizarListaDeAgengicas()
+        {
             lstAgencias.Items.Clear();
             var agencias = _contextoBancoDeDados.Agencias.ToList();
             foreach (var agencia in agencias)
                 lstAgencias.Items.Add(agencia);
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var agenciaSelecionada = (Agencia)this.lstAgencias.SelectedItem;
+
+            txtNumero.Text = agenciaSelecionada.Numero;
+            txtNome.Text = agenciaSelecionada.Nome;
+            txtTelefone.Text = agenciaSelecionada.Telefone;
+            txtEndereco.Text = agenciaSelecionada.Endereco;
+            txtDescricao.Text = agenciaSelecionada.Descricao;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
